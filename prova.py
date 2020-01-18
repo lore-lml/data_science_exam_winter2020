53297a9711a0f5f1d93e40f0c492ac19ed1c319f
lore-lml <lorenzo.limoli@studenti.polito.it>
2020-01-18 17:54:13 +0100

    Gaussian

--- prova.py
+++ prova.py
@@ -1,4 +1,4 @@
 import re
-text = "..ciao ..lo *come *****ahsahha ..akjsldklas . .. *** come..come -cici ahah-chch"
+text = "..ciao ..lo *come *****ahsahha ..akjsldklas . .. *** come..come -cici ahah-chch a++++"
 #print(re.sub(r"([\\*\\.\-/])+[.]*", r"\0", text))
-print(re.sub("([*.\-/])", " ", text))
+print(re.sub("([*.\-/+])", " ", text))
