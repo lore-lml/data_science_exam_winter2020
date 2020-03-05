f2020cd2b3e329bb3083f02a0e9d57e0e2b24175
Lorenzo Limoli <lorenzo.limoli@outlook.it>
2020-03-05 20:38:06 +0100

    Final solution

--- prova.py
+++
@@ -1,8 +0,0 @@
-from nltk.stem.snowball import ItalianStemmer
-from nltk.stem import SnowballStemmer
-from nltk.stem.wordnet import WordNetLemmatizer
-stem = SnowballStemmer(language='italian')
-lem = WordNetLemmatizer()
-print(lem.lemmatize("abbandonata"))
-print(stem.stem("abbandonata"))
-print(stem.stem("abbandonerà"))
